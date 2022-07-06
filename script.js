89abcd442bf8a98fd98c1011d5f8366a949071c6
Paula Cassia Feltrin <feltrin.paulacassia@gmail.com>
2022-07-06 09:11:28 -0300

    Desafio finalizado

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const [input1, input2] = document.querySelectorAll("input");
-const botao = document.querySelector(".button");
-//const imagem = document.querySelector(".img-lol");
+const botao = document.querySelector("button");
 
 function logar(){
     if(input1.value && input2.value.length >= 8){
@@ -8,20 +7,8 @@ function logar(){
         botao.classList.add("ativo");
     }else{
         botao.setAttribute("disabled", "disabled");
-        //imagem.classList.remove("ativo");
     }
 }
 
 input1.addEventListener("input", logar);
 input2.addEventListener("input", logar);
-
-
-//bonus
-// function corDeFundo(event){
-//     if(event.key === 'v' || event.key === "V"){
-//         document.body.classList.toggle("fundo"); //TOGGLE -> adiciona e remove
-//     }
-    
-// }
-
-// window.addEventListener("keydown", corDeFundo);
