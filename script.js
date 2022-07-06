a9fceb70ad70a90cb6fd8e86fb089f9c2b4acb12
Paula Cassia Feltrin <feltrin.paulacassia@gmail.com>
2022-07-06 09:02:05 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const [input1, input2] = document.querySelectorAll("input");
-const botao = document.querySelector(".seta");
-const imagem = document.querySelector(".img-lol");
+const botao = document.querySelector(".button");
+//const imagem = document.querySelector(".img-lol");
 
-function mudanca(){
+function logar(){
     if(input1.value && input2.value.length >= 8){
         botao.removeAttribute("disabled");
-        imagem.classList.add("ativo");
+        botao.classList.add("ativo");
     }else{
         botao.setAttribute("disabled", "disabled");
-        imagem.classList.remove("ativo");
+        //imagem.classList.remove("ativo");
     }
 }
 
-input1.addEventListener("input", mudanca);
-input2.addEventListener("input", mudanca);
+input1.addEventListener("input", logar);
+input2.addEventListener("input", logar);
 
 
 //bonus
