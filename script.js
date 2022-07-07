c929f8c2105784166cb664f693c7c97b8a306bc7
Paula Cassia Feltrin <feltrin.paulacassia@gmail.com>
2022-07-07 08:48:53 -0300

    Finalizado

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const botao = document.querySelector("button");
 function logar(){
     if(input1.value && input2.value.length >= 8){
         botao.removeAttribute("disabled");
-        botao.classList.add("ativo");
+        botao.classList.toggle("ativo");
     }else{
         botao.setAttribute("disabled", "disabled");
     }
@@ -12,3 +12,29 @@ function logar(){
 
 input1.addEventListener("input", logar);
 input2.addEventListener("input", logar);
+
+
+// const [input1, input2] = document.querySelectorAll("input");
+// const imagem = document.querySelector(".img-lol");
+// const botao = document.querySelector("button");
+// function mudanca(){
+//     if(input1.value && input2.value.length >= 8){
+//         botao.removeAttribute("disabled");
+//         imagem.classList.add("ativo");
+//     }else{
+//         botao.setAttribute("disabled", "disabled");
+//         imagem.classList.remove("ativo");
+//     }
+// }
+// input1.addEventListener("input", mudanca);
+// input2.addEventListener("input", mudanca);
+
+// //bonus
+// function corDeFundo(event){
+//     if(event.key === 'v' || event.key === "V"){
+//         document.body.classList.toggle("fundo"); //TOGGLE -> adiciona e remove
+//     }
+    
+// }
+
+// window.addEventListener("keydown", corDeFundo);
